7325899f743e57da825cd685011638bb38244717
Dheeraj <dheerajraina55@gmail.com>
2023-06-27 13:52:26 +0530

    function to store all emails, phone numbers and primary ids into maps

--- src/controllers/identity_controller.ts
+++ src/controllers/identity_controller.ts
@@ -11,6 +11,41 @@ let linked_emails_set: Set<string> = new Set();
 let linked_phone_numbers_set: Set<string> = new Set();
 let secondary_contact_ids: Set<number> = new Set();
 
+// stores all the emails,phone numbers,primary ids
+async function store_all_phonenumbers_and_emails_and_primary_ids(
+	all_contacts
+): Promise<void> {
+	await all_contacts.forEach((contact) => {
+		let contact_details = contact.dataValues;
+		if (contact_details.phoneNumber) {
+			let phone_number = contact_details.phoneNumber;
+			if (!phone_number_store.has(phone_number)) {
+				phone_number_store.set(phone_number, true);
+			}
+		}
+		if (contact_details.email) {
+			let email = contact_details.email;
+			if (!email_store.has(email)) {
+				email_store.set(email, true);
+			}
+		}
+		if (contact_details.linkPrecedence == "primary") {
+			let primary_id: number = contact_details.id;
+			if (!primary_id_store.has(primary_id)) {
+				primary_id_store.set(primary_id, true);
+			}
+		} else {
+			let primary_id: number = contact_details.linkedId;
+			if (!primary_id_store.has(primary_id)) {
+				primary_id_store.set(primary_id, true);
+			}
+		}
+	});
+
+	return;
+}
+
+
 
 function clear_all_globals() {
 	phone_number_store.clear();
